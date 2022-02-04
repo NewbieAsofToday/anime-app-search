f78a49c0a30a99514985478af6abce004a4cba0c
Azk <azkriven17@gmail.com>
2022-02-04 11:22:21 +0800

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
 const Navbar = ({ handleSearch, search, setSearch }) => {
   return (
-    <>
-      <h1>AniSearch</h1>
+    <Wrapper>
+      <h1>
+        Ani<span>Search</span>
+      </h1>
       <form onSubmit={handleSearch}>
         <input
           type='text'
@@ -11,10 +14,37 @@ const Navbar = ({ handleSearch, search, setSearch }) => {
           onChange={(e) => {
             setSearch(e.target.value);
           }}
+          placeholder='Search for Anime...'
         />
+        <FaSearch className='logo' />
       </form>
-    </>
+    </Wrapper>
   );
 };
-
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #333;
+  padding: 0 2rem;
+  height: 10vh;
+  h1 {
+    color: #fff;
+    span {
+      color: #ccc;
+    }
+  }
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    input {
+      font-size: 1.2rem;
+    }
+    .logo {
+      color: white;
+      font-size: 2rem;
+    }
+  }
+`;
 export default Navbar;
